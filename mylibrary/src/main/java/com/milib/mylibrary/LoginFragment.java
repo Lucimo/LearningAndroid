ed8cff5b52958b910a1f0ea4107364a22a294f54
Lucas <lucas.pajares@gmail.com>
2018-09-17 12:48:11 +0200

    CreandoLogin

---
+++ mylibrary/src/main/java/com/milib/mylibrary/LoginFragment.java
@@ -0,0 +1,55 @@
+package com.milib.mylibrary;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LoginFragment extends Fragment {
+public EditText etemail;
+public EditText etpass;
+public Button btnLogin;
+public Button btnRegister;
+public LoginFragmentEvents events;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       View v=inflater.inflate(R.layout.fragment_login, container, false);
+       //devuelve la representacion en objeto del xml, lo guardamos en v
+
+        etemail = v.findViewById(R.id.etemail);
+        etpass = v.findViewById(R.id.etpass);
+        btnLogin = v.findViewById(R.id.btnLogin);
+        btnRegister = v.findViewById(R.id.btnRegister);
+        events = new LoginFragmentEvents(this);
+        btnLogin.setOnClickListener(events);
+        btnRegister.setOnClickListener(events);
+        return v;
+    }
+
+}
+class LoginFragmentEvents implements View.OnClickListener{
+    private LoginFragment loginFragment;
+public LoginFragmentEvents(LoginFragment fragment){
+this.loginFragment = fragment;
+}
+    @Override
+    public void onClick(View view) {
+
+    }
+}
